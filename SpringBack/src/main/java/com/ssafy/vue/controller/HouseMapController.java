e6c118506a0f6e23154a0d26f1b1236328c5f9a2
Leeh9 <gusrnqq@naver.com>
2022-11-24 12:59:06 +0900

    1124 이현구 스프링 수정

--- SpringBack/src/main/java/com/ssafy/vue/controller/HouseMapController.java
+++ SpringBack/src/main/java/com/ssafy/vue/controller/HouseMapController.java
@@ -3,9 +3,7 @@ package com.ssafy.vue.controller;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.List;
@@ -14,23 +12,15 @@ import org.json.JSONObject;
 import org.json.XML;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.ssafy.vue.model.HouseInfoDto;
-import com.ssafy.vue.model.SidoGugunCodeDto;
-import com.ssafy.vue.model.service.HouseMapService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 
 @RestController
 @RequestMapping("/map")
@@ -38,24 +28,6 @@ import io.swagger.annotations.ApiParam;
 public class HouseMapController {
 
 	private final Logger logger = LoggerFactory.getLogger(HouseMapController.class);
-
-	@Autowired
-	private HouseMapService haHouseMapService;
-
-	@ApiOperation(value = "시도 정보", notes = "전국의 시도를 반환한다.", response = List.class)
-	@GetMapping("/sido")
-	public ResponseEntity<List<SidoGugunCodeDto>> sido() throws Exception {
-		logger.info("sido - 호출");
-		return new ResponseEntity<List<SidoGugunCodeDto>>(haHouseMapService.getSido(), HttpStatus.OK);
-	}
-
-	@ApiOperation(value = "구군 정보", notes = "전국의 구군을 반환한다.", response = List.class)
-	@GetMapping("/gugun")
-	public ResponseEntity<List<SidoGugunCodeDto>> gugun(
-			@RequestParam("sido") @ApiParam(value = "시도코드.", required = true) String sido) throws Exception {
-		logger.info("gugun - 호출");
-		return new ResponseEntity<List<SidoGugunCodeDto>>(haHouseMapService.getGugunInSido(sido), HttpStatus.OK);
-	}
 
 	@ApiOperation(value = "아파트 목록", notes = "지역코드와 매매계약월을 기준으로 아파트 목록을 반환한다.", response = List.class)
 	@GetMapping(value = "/searchApt/{lawd_cd}/{deal_ymd}", produces = "application/json;charset=utf-8")
